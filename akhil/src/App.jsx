0b306873884444c7286811e355a993837e91dcb4
akhilk49 <akhilkkurian49@gmail.com>
2024-01-08 10:28:21 +0530

    okk

---
+++ akhil/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import HigherorderFunction from './component/HigherOrderFunctions'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <HigherorderFunction />
+
+      </div>
+    </>
+  )
+}
+
+export default App
